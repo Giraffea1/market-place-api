3c842ad2d1f3473c0cae8c4a88d13a7b8fb8c9a8
Giraffea1 <58932648+Giraffea1@users.noreply.github.com>
2025-07-16 15:00:26 -0500

    setup project

---
+++ src/main/java/com/jhu/enterprise/market_place_api/MarketPlaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.jhu.enterprise.market_place_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhu/enterprise/market_place_api/MarketPlaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhu.enterprise.market_place_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
